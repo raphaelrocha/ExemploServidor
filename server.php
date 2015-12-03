2bc31da13b4d0d18bd7b598194fc71f1732627c8
Raphael Rocha <raphaelrocha86@gmail.com>
2015-12-03 13:53:31 -0400

    Agora, um objeto Json é recebido como valor para a chave "json" dentro do array $_POST.

--- server.php
+++ server.php
@@ -1,24 +1,30 @@
 <?php 
 header ('Content-type: text/html; charset=UTF-8');
 
+if(isset($_POST['json'])){
 
+	$json = json_decode($_POST["json"]);
 
-if(isset($_POST['method'])){
-
-	if(strcmp('object', $_POST['method']) == 0){
-		$method = $_POST['method'];
-		$dados = $_POST['data'];
-		$mensagem_para_enviar = "MEDOTO: ".$method." | DADOS: ".$dados;
-		echo json_encode(array('mensagem'=>$mensagem_para_enviar));
+	//if(strcmp('object', $_POST['method']) == 0){
+	if($json->method=="object"){
+		$method = $json->method;
+		$dados = $json->data;
+		$mensagem_para_enviar = "Isso é um JsonObject";
+		echo json_encode(array('method'=>$method,
+							   'to_server'=>$dados,
+							   'from_server'=>$mensagem_para_enviar));
 	}
 
-	else if(strcmp('array', $_POST['method']) == 0){
-		$method = $_POST['method'];
-		$dados = $_POST['data'];
-		$mensagem_para_enviar = "METODO: ".$method." | DADOS: ".$dados;
+	//else if(strcmp('array', $_POST['method']) == 0){
+	else if($json->method=="array"){
+		$method = $json->method;
+		$dados = $json->data;
+		$mensagem_para_enviar = "Isso é um JsonArray";
 
 		$array = array();
-		array_push($array, array('mensagem'=>$mensagem_para_enviar)); 
+		array_push($array, array('method'=>$method,
+								 'to_server'=>$dados,
+								 'from_server'=>$mensagem_para_enviar)); 
 		
 		$array_final = array();
 		array_push($array_final, $array);
@@ -32,5 +38,4 @@ if(isset($_POST['method'])){
 }else{
 	echo "erro geral";
 }
-
  ?>
